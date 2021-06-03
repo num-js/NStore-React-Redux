9d65e64f3cc00183d5c340c336417234b808f705
Numan Ahmed <mdnmnahmed@gmail.com>
2021-06-03 09:06:51 +0530

    product actions

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,15 @@
+import { ActionTypes } from '../actionContants/productActionTypes';
+
+export const setProducts = (products) => {
+    return {
+        type: ActionTypes.SET_PRODUCTS,
+        payload: products
+    }
+}
+
+export const selectedProduct = (product) => {
+    return {
+        type: ActionTypes.SELECTED_PRODUCT,
+        payload: product
+    }
+}
